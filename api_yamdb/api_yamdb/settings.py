90e17b22a5bdf61a28eb919918b3c6fb819b81a1
Игорь Живов <izhivov97@yandex.ru>
2022-07-09 22:21:27 +0300

    перенес статику, исправил путь до статики

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -101,7 +101,7 @@ USE_TZ = False
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR + '/api/static/']
+STATICFILES_DIRS = [BASE_DIR + '/api/static/']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
